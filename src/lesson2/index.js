96e7eb80852960b3fc9801f6f9f699c118ae9fd4
Hao Zhu <hao.zhu@thoughtworks.com>
2020-01-05 15:01:48 +0800

    Submit homework

--- src/lesson2/index.js
+++ src/lesson2/index.js
@@ -1,38 +1,43 @@
 export const StatusEnum = {
   available: 1,
   unavailable: 0,
-};
+}
 
-/// 随机获取box的id
-export const getRandomInt = (m) => {
-  return parseInt(Math.random()*m, 10);
-};
+// / 随机获取box的id
+export const getRandomInt = (m) => parseInt(Math.random() * m, 10)
 
-/// 获取box的临时的取货码
+// / 获取box的临时的取货码
 export const getRandomString = (boxId) => {
-  let currentDate = new Date();
-  let dateStr = currentDate.format("yyyyMMddHHmmss");
-  const randomStr = Math.random().toString();
-  return dateStr + boxId + randomStr;
-};
+  const currentDate = new Date()
+  const dateStr = currentDate.format('yyyyMMddHHmmss')
+  const randomStr = Math.random().toString()
+  return dateStr + boxId + randomStr
+}
 
-export const STORE_PACKAGE_ERROR = "No available box.";
-export const GET_PACKAGE_ERROR = "The barcode is invalid!";
-export const GET_PACKAGE_SUCCESS = "Scan barcode success.";
+export const STORE_PACKAGE_ERROR = 'No available box.'
+export const GET_PACKAGE_ERROR = 'The barcode is invalid!'
+export const GET_PACKAGE_SUCCESS = 'Scan barcode success.'
 
-
-Date.prototype.format = function (fmt) {
-  var o = {
-      "M+": this.getMonth() + 1, //月份
-      "d+": this.getDate(), //日
-      "h+": this.getHours(), //小时
-      "m+": this.getMinutes(), //分
-      "s+": this.getSeconds(), //秒
-      "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-      "S": this.getMilliseconds() //毫秒
-  };
-  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-  for (var k in o)
-      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-  return fmt;
+Date.prototype.format = function(fmt) {
+  const o = {
+    'M+': this.getMonth() + 1, // 月份
+    'd+': this.getDate(), // 日
+    'h+': this.getHours(), // 小时
+    'm+': this.getMinutes(), // 分
+    's+': this.getSeconds(), // 秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+    S: this.getMilliseconds(), // 毫秒
+  }
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      `${this.getFullYear()}`.substr(4 - RegExp.$1.length)
+    )
+  for (const k in o)
+    if (new RegExp(`(${k})`).test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : `00${o[k]}`.substr(`${o[k]}`.length)
+      )
+  return fmt
 }
--- src/lesson2/index.test.js
+++ src/lesson2/index.test.js
@@ -1,40 +1,40 @@
 import Locker from './Locker'
-import { STORE_PACKAGE_ERROR, GET_PACKAGE_SUCCESS, GET_PACKAGE_ERROR } from '.';
+import { STORE_PACKAGE_ERROR, GET_PACKAGE_SUCCESS, GET_PACKAGE_ERROR } from '.'
 
 describe('Locker test', () => {
   it('should generate a barcode when store package given an available locker', () => {
-    const locker = new Locker(24);
+    const locker = new Locker(24)
 
-    const result = locker.storePackage();
+    const result = locker.storePackage()
 
-    expect(result).not.toEqual(STORE_PACKAGE_ERROR);
+    expect(result).not.toEqual(STORE_PACKAGE_ERROR)
   })
 
   it('should show error message when store package given an locker without empty box', () => {
-    const locker = new Locker(0);
+    const locker = new Locker(0)
 
-    const result = locker.storePackage();
+    const result = locker.storePackage()
 
-    expect(result).toEqual(STORE_PACKAGE_ERROR);
+    expect(result).toEqual(STORE_PACKAGE_ERROR)
   })
 
   it('should open the locker when get package given a valid barcode', () => {
-    const locker = new Locker(24);
+    const locker = new Locker(24)
 
-    const barcode = locker.storePackage();
+    const barcode = locker.storePackage()
 
-    const result = locker.getPackage(barcode);
+    const result = locker.getPackage(barcode)
 
-    expect(result).toEqual(GET_PACKAGE_SUCCESS);
+    expect(result).toEqual(GET_PACKAGE_SUCCESS)
   })
 
   it('should show error message when get package given an invalid barcode', () => {
-    const locker = new Locker(24);
+    const locker = new Locker(24)
 
-    locker.storePackage();
+    locker.storePackage()
 
-    const result = locker.getPackage("32432432432");
+    const result = locker.getPackage('32432432432')
 
-    expect(result).toEqual(GET_PACKAGE_ERROR);
+    expect(result).toEqual(GET_PACKAGE_ERROR)
   })
 })
